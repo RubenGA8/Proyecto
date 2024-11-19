dedd9ae3029a0e060ffa0937e1464af8b7299ab9
RubenGA8 <127164597+RubenGA8@users.noreply.github.com>
2024-11-18 21:03:40 -0600

    Update resultados.tsx

--- app/resultados.tsx
+++ app/resultados.tsx
@@ -1,12 +1,15 @@
 import { Text, View, Pressable, Button } from 'react-native';
 import { Link, router } from 'expo-router';
 import { Estilos } from "@/constants/Styles";
+import { useState, useContext} from 'react'
 import { ContextEncuesta } from './context';
 
 export default function Index() {
+    const {encuesta, setEncuesta} = useContext(ContextEncuesta);
+
     return(
         <View>
-            <Text>resultados</Text>
+            <Text>resultados buenos</Text>
         </View>
     );
 }
